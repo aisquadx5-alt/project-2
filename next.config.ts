4eb8d392e2094c03bd965893ee080adea6b1832b
SaaS Developer <developer@example.com>
2026-06-21 01:28:07 +0500

    Relax-CSP-headers-to-allow-unsafe-inline-and-unsafe-eval

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *; script-src 'self' 'unsafe-inline' 'unsafe-eval';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
